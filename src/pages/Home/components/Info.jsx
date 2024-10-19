e6af59e9f5b8bbac1029c1d811842f0dfb5b3287
arnabara4 <arnabjena4@gmail.com>
2024-10-19 23:25:56 +0530

    debug

--- src/pages/Home/components/Info.jsx
+++ src/pages/Home/components/Info.jsx
@@ -5,7 +5,7 @@ const Info = () => {
   return (
     <div className={styles.book}>
         <ul>
-            <li className={styles.name}>STEP INN, KASOL</li>
+            <li className={styles.name}>TAJ HOTEL</li>
             <li className={styles.date}>04 Sep 2024 - 05 Sep 2024</li>
             <li className={styles.info}>1 Guest - 1 Room</li>
             <li className={styles.misc}>Special Co... &#9662;</li>
